e579369e2c5fe2d815a2395b230ad2f7294d8b92
Mohamed Hesham <medo93346@gmail.com>
2025-11-24 14:14:54 +0200

    Feature: add animation for progressbar using js

---
+++ main.js
@@ -0,0 +1,10 @@
+let progressSpans = document.querySelectorAll(".the-progress span");
+let section = document.querySelector(".our-skills");
+
+window.onscroll = function (){
+    if (window.scrollY >= section.offsetTop - 250) {
+        progressSpans.forEach((span) => {
+        span.style.width = span.dataset.width;
+        });
+    }
+};
