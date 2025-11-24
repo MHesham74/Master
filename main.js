7b99589cd844d48f279b7001cd8cacda597820ed
Mohamed Hesham <medo93346@gmail.com>
2025-11-24 15:28:54 +0200

    Feature: set countdown timer in events section

--- main.js
+++ main.js
@@ -8,3 +8,24 @@ window.onscroll = function (){
         });
     }
 };
+
+let countDownDate = new Date("Dec 31,2026 23:59:59").getTime();
+
+let counter = setInterval(() =>{
+    let dateNow = new Date().getTime();
+    let dateDiff = countDownDate - dateNow;
+
+    let days = Math.floor(dateDiff / ( 1000 * 60 * 60 * 24));
+    let hours = Math.floor((dateDiff % ( 1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    let minutes = Math.floor(dateDiff % ( 1000 * 60 * 60) / (1000 * 60));
+    let seconds = Math.floor(dateDiff % ( 1000 * 60 ) / 1000);
+    
+    document.querySelector(".unit .days").innerHTML = days;
+    document.querySelector(".unit .hours").innerHTML = hours;
+    document.querySelector(".unit .minutes").innerHTML = minutes < 10 ? `0${minutes}` : minutes;
+    document.querySelector(".unit .seconds").innerHTML = seconds < 10 ? `0${seconds}` : seconds;
+
+    if(dateDiff === 0){
+        clearInterval(counter);
+    }
+},1000);
